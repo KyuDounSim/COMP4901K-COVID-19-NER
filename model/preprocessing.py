6e3de893e2a54474048d2632e811c5f950074c1a
Choi Sehyun <schoiaj@connect.ust.hk>
2020-11-11 01:37:38 +0900

    Added preliminary bert model and training code

---
+++ model/preprocessing.py
@@ -0,0 +1,8 @@
+"""A module dedicated to preprocessing the dataset.
+
+BERT requires specific data input types: need to conform to it.
+Also, we need to use the same bert_tokenizer used for the bert model we are
+using.
+"""
+
+# TODO: Implement this file
